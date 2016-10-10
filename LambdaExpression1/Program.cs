c3944239f8e20c9e3da5178304ac58a37eca83d1
Brad Kilkenney <bkilkenney@gmail.com>
2016-10-10 17:37:11 -0400

    Initial commit

--- LambdaExpression1/Program.cs
+++ LambdaExpression1/Program.cs
@@ -10,16 +10,20 @@ namespace LambdaExpression1
     {
         static void Main(string[] args)
         {
-            List<Employee> listEmployees = new List<Employee>();
+            List<Employee> listEmployees = new List<Employee>()
             {
-                new Employee { ID = 101, Name = "Brad" };
-                new Employee { ID = 102, Name = "Carol" };
-                new Employee { ID = 103, Name = "Doris" };
-                new Employee { ID = 104, Name = "Edgar" };
+                new Employee { ID = 101, Name = "Brad" },
+                new Employee { ID = 102, Name = "Carol" },
+                new Employee { ID = 103, Name = "Doris" },
+                new Employee { ID = 104, Name = "Edgar" },
             };
 
-            Employee employee = listEmployees.Find(Emp => Emp.ID == 101);
-            Console.WriteLine("ID: {0}, Name: {1}", employee.ID, employee.Name);  //This is not linked to an object(employee)
+            Employee employee = listEmployees.Find(Emp => Emp.ID == 101);  //EMP is anonymous method of type Employee
+                                                                           //Find method being invoked on listEmployees object
+                                                                          //Emp is inferred to be a listEmployees object
+                                                                          //This expression is ultimately saying find me employee ID 101
+
+            Console.WriteLine("The employee is {0} and the employee's ID is {1}", employee.Name, employee.ID);  //This is not linked to an object(employee)
             
         }
        
